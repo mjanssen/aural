2ff829ec343e6e4bf7d9491525632bce8d246709
mjanssen <dev.marnix@gmail.com>
2018-04-01 00:31:51 +0200

    Updated methods for result, added comments

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
+/**
+ * The Aural class, wohoo
+ */
 class Aural {
   constructor() {
     const AudioContext = window.AudioContext || window.webkitAudioContext;
@@ -7,81 +10,137 @@ class Aural {
     this.sources = {};
   }
 
+  /**
+   * Create the audio object with the loaded buffer
+   * @param {string} key
+   * @param {audioBuffer} buffer
+   * @param {object} options
+   */
   newBufferSource(key, buffer = false, options) {
     if (buffer === false) {
       return null;
     }
 
-    this.sources[key] = this.context.createBufferSource();
+    this.sources[key] = {};
+
     this.sources[key].options = options;
     this.sources[key].isPlaying = false;
 
-    this.sources[key].buffer = buffer;
-    this.sources[key].analyzer = {
+    this.sources[key].audio = this.context.createBufferSource();
+
+    this.sources[key].audio.buffer = buffer;
+    this.sources[key].audio.analyzer = {
       node: this.context.createAnalyser(),
       bufferLength: null,
       dataArray: null,
     };
 
-    this.sources[key].connect(this.sources[key].analyzer.node);
-    this.sources[key].analyzer.node.connect(this.context.destination);
-    this.sources[key].playbackRate.value = 0;
+    this.sources[key].audio.connect(this.sources[key].audio.analyzer.node);
+    this.sources[key].audio.analyzer.node.connect(this.context.destination);
+    this.sources[key].audio.playbackRate.value = 0;
 
-    this.sources[key].analyzer.bufferLength = this.sources[key].analyzer.node.frequencyBinCount;
+    this.sources[key].audio.analyzer.bufferLength = this.sources[
+      key
+    ].audio.analyzer.node.frequencyBinCount;
 
-    this.sources[key].analyzer.dataArray = new Uint8Array(this.sources[key].analyzer.bufferLength);
+    this.sources[key].audio.analyzer.dataArray = new Uint8Array(
+      this.sources[key].audio.analyzer.bufferLength
+    );
 
-    this.sources[key].start(0);
+    this.sources[key].audio.start(0);
 
     if (options.autoPlay) {
       this.sources[key].isPlaying = true;
-      this.sources[key].playbackRate.value = options.rate || 1.0;
+      this.sources[key].audio.playbackRate.value = options.rate || 1.0;
+    }
+
+    this.sources[key].getFrequency = () => this.getFrequency(key);
+    this.sources[key].play = () => this.play(key);
+    this.sources[key].pause = () => this.pause(key);
+    this.sources[key].stop = () => this.stop(key);
+    this.sources[key].setRate = rate => this.setRate({ key, rate }, true);
+
+    if (options.onLoad) {
+      options.onLoad(this.sources[key]);
     }
 
     return this.sources[key];
   }
 
+  /**
+   * Fetch the source file and parse the buffer, passing it to newBufferSource()
+   * @param {string} source
+   */
   getBuffer(source) {
     return fetch(source)
       .then(result => result.arrayBuffer())
       .then(arrayBuffer => this.context.decodeAudioData(arrayBuffer, buffer => buffer));
   }
 
+  /**
+   * Load the audio file and create the Aural object
+   * @param {string} key
+   * @param {string} source
+   * @param {object} options
+   */
   load(key, source, options) {
-    this.getBuffer(source).then(buffer => this.newBufferSource(key, buffer, options));
+    return this.getBuffer(source).then(buffer => this.newBufferSource(key, buffer, options));
   }
 
+  /**
+   * Play audio file
+   * @param {string} key
+   */
   play(key) {
-    if (typeof this.sources[key] !== 'undefined' && this.sources[key].isPlaying === false) {
-      const { options } = this.sources[key];
-      this.sources[key].playbackRate.value = options.rate;
-    }
+    this.setRate({ key, rate: this.sources[key].options.rate, isPlaying: true });
   }
 
+  /**
+   * Pause audio file
+   * @param {string} key
+   */
   pause(key) {
-    this.setRate(key, 0);
+    this.setRate({ key, rate: 0, isPlaying: false });
   }
 
+  /**
+   * Stop audio file
+   * @param {string} key
+   */
   stop(key) {
-    if (typeof this.sources[key] !== 'undefined' && this.sources[key].isPlaying) {
-      this.sources[key].stop();
-      const { buffer, options } = this.sources[key];
+    if (typeof this.sources[key] !== 'undefined') {
+      this.sources[key].audio.stop();
+      const options = this.sources[key].options;
+      options.autoPlay = 0;
+      const buffer = this.sources[key].audio.buffer;
+
       this.newBufferSource(key, buffer, options);
     }
   }
 
-  setRate(key, rate) {
-    if (typeof this.sources[key] !== 'undefined') {
-      this.sources[key].options.rate = rate;
-      this.sources[key].playbackRate.value = rate;
+  /**
+   * Change the rate for the audio object
+   * @param {object} options
+   * @param {bool} updateOptions
+   */
+  setRate(options, updateOptions = false) {
+    if (options.key && typeof this.sources[options.key] !== 'undefined') {
+      updateOptions && (this.sources[options.key].options.rate = options.rate);
+      this.sources[options.key].audio.playbackRate.value = options.rate;
     }
   }
 
+  /**
+   * Get the frequency of a playing sound
+   * @param {string} key
+   */
   getFrequency(key) {
     if (typeof this.sources[key] !== 'undefined') {
-      this.sources[key].analyzer.node.getByteFrequencyData(this.sources[key].analyzer.dataArray);
+      this.sources[key].audio.analyzer.node.getByteFrequencyData(
+        this.sources[key].audio.analyzer.dataArray
+      );
       return (
-        this.sources[key].analyzer.dataArray.reduce(reducer) /
+        this.sources[key].audio.analyzer.dataArray.reduce(reducer) /
         (this.sources[key].options.frequencyDevider || 128)
       );
     }
